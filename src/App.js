bedaa1c937e52a86699e870f2ce4fdbd46f0a1a4
Babuprasanth R <117240764+Babuprasanth27@users.noreply.github.com>
2024-04-20 13:43:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TicTactToe from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <TicTactToe/>
+    </div>
+  );
+}
+
+export default App;
